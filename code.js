5c18de5ae632ef4f496a925a3d68c16f77883bbf
Kerui-C <keruichen2@gmail.com>
2023-12-29 15:24:00 -0500

    yep

--- code.js
+++ code.js
@@ -1,2 +1,26 @@
-
-
+window.addEventListener('load', function () {
+    const paper = document.getElementById("paper");
+    const rock = document.getElementById("rock");
+    const scissors = document.getElementById("scissors");
+    const user = document.querySelector(".user-choice");
+    const comp = document.querySelector(".comp-choice");
+    const selection = [rock, paper, scissors]
+  
+    function clickHandler(button) {
+    const contents = document.createElement("p");
+    contents.classList.add('contents');
+    contents.textContent = button.innerHTML;
+    contents.style.fontSize = "100px";
+    user.appendChild(contents);
+    const comp_contents = selection[Math.floor(Math.random() * 3)];
+    }
+    paper.addEventListener('click', function () {
+        clickHandler(this);
+    });
+    rock.addEventListener('click', function () {
+        clickHandler(this);
+    });
+    scissors.addEventListener('click', function () {
+        clickHandler(this);
+    });
+});
