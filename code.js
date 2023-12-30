7ee1c436bb87d4ceebb55dc4bb6ee601f6a3d5a7
Kerui-C <keruichen2@gmail.com>
2023-12-30 06:23:52 -0500

    GLORY TO CHINA

--- code.js
+++ code.js
@@ -4,15 +4,56 @@ window.addEventListener('load', function () {
     const scissors = document.getElementById("scissors");
     const user = document.querySelector(".user-choice");
     const comp = document.querySelector(".comp-choice");
-    const selection = [rock, paper, scissors]
+    const selection = [rock, paper, scissors];
+    const pscore = { contents: "0" };
+    const cscore = { contents: "0" };
   
+    //function handleResults(){}
+
     function clickHandler(button) {
+    user.innerHTML = '';
     const contents = document.createElement("p");
     contents.classList.add('contents');
     contents.textContent = button.innerHTML;
     contents.style.fontSize = "100px";
+    contents.style.margin = "0";
     user.appendChild(contents);
-    const comp_contents = selection[Math.floor(Math.random() * 3)];
+    comp.innerHTML = '';
+    const comp_contents_container = document.createElement("p")
+    const choice = selection[Math.floor(Math.random() * 3)]
+    comp_contents_container.textContent = choice.innerHTML;
+    comp_contents_container.style.fontSize = "100px";
+    comp_contents_container.style.margin = "0";
+    comp.appendChild(comp_contents_container);
+    switch(contents.textContent+comp_contents_container.textContent){
+        case "✌✊":
+        case "✊✋":
+        case "✋✌":
+            cscore.contents = String(Number(cscore.contents) + 1);
+            document.querySelector('.computer .cscore').textContent = cscore.contents;
+            break;
+        case "✊✊":
+        case "✋✋":
+        case "✌✌":
+            break;
+        case "✌✋":
+        case "✊✌":
+        case "✋✊":
+            pscore.contents = String(Number(pscore.contents) + 1);
+            document.querySelector('.user .pscore').textContent = pscore.contents;
+            break;
+        };
+        if(Number(pscore.contents)>=5){
+            document.getElementById("win").textContent = "YOU WIN!";
+            var audio = new Audio('_Victory Song_ - Chinese Patriotic Song.mp3');
+            audio.play();
+
+        }
+        if(Number(cscore.contents)>=5){
+            document.getElementById("win").textContent = "YOU LOSE!";
+            var audio = new Audio('mixkit-crowd-disappointment-long-boo-463.wav');
+            audio.play();
+        }
     }
     paper.addEventListener('click', function () {
         clickHandler(this);
